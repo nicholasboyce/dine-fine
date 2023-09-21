7796295e262e3779944968bf6d0421a433aa1768
nicholasboyce <nicholasboyce@protonmail.com>
2023-09-21 15:16:39 -0400

    Initial commit for restaurant page. Practicing webpack.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import Background from './foodbackground.jpg';
+import './style.css';
+
+console.log("Jump!");
+
+const myBackground = new Image();
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 3000,
+    static: {
+        directory: path.join(__dirname, './dist'),
+        publicPath: '/lastcache',
+    },
+    server: 'http',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+    ],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    splitChunks: {
+        chunks: 'all',
+    },
+  },
+};
